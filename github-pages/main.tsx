cc5949c54b62f7fbc470ec4ad4a44ed9d1dc43df
f6yv7sgtgw-wq <f6yv7sgtgw@privaterelay.appleid.com>
2026-08-10 11:40:17 +0200

    Load Project SNES 0.3.5.2 on GitHub Pages

--- github-pages/main.tsx
+++ github-pages/main.tsx
@@ -7,6 +7,7 @@ import catalog from "../app/snes-games.json";
 import "../app/globals.css";
 import "../app/ui-v035.css";
 import "../app/ui-v0351.css";
+import "../app/ui-v0352.css";
 
 const baseUrl = import.meta.env.BASE_URL;
 const games = (catalog.games as Game[]).map((game) => ({
